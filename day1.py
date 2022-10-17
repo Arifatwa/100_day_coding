9242a6e5b426ac9e6a138e775f43a53ea1e73a1a
ADRIAWAN01 <hermanadriawan@gmail.com>
2022-10-18 07:12:25 +0800

    Day 02

---
+++ day1.py
@@ -0,0 +1,34 @@
+# variabel adalah tempat menyimpan data 
+a = 10  # a adalah variabel integer & 10 adalah nili dari a.
+b = 50 
+
+panjang = 100
+
+
+# pemanggilan ke 1
+print("nilai a = ", a)
+print("nilai b = ", b)
+print("nilai panjang = ", a)
+print("_________________")
+
+# penamaan 
+# nilai y = 27 tidak boleh
+nilaiy  = 15 # dengan menggunakan underscore
+# 10rubu = 100 tidak boleh
+ribu10 = 18 # boleh
+
+nilaiZ = 82 # boleh
+
+# pemanggilan ke 2
+print("nilai a = ", a)
+a = 9
+print("nilai a = ", a)
+print("_________________")
+
+# assigment indirect
+b = a
+print("nilai b = ", b)
+print("_________________")
+
+
+
---
+++ day2.py
@@ -0,0 +1,32 @@
+# mengenal type data pada pytohon 
+data_string = "arifatwa" # type data string atau karakter
+print("data : ", data_string)
+print("- bertype data : ", type(data_string))
+
+data_integer = 100 # type data integer atau bilangan bulat 
+print("data : ", data_integer)
+print("- bertype data : ", type(data_integer))
+
+data_float = 10.1 #type data float atau bilangan pecahan
+print("data : ", data_float)
+print("- bertype data : ", type(data_float))
+
+data_boolean = True # type data boolean atau True & False (benar/salah)
+print("data : ", data_boolean)
+print("- bertype data : ", type(data_boolean))
+print("_______________________________________")
+
+nama = "arifatwa"
+umur = 19
+tinggi = 160.3
+mahasiswa = True
+
+print("nama saya adalah : ", nama)
+print("umur  : ", umur)
+print("tinngi badan : ", tinggi)
+print("apakah benar anda mahasiswa? : ", mahasiswa)
+print("_______________________________________")
+
+
+
+
