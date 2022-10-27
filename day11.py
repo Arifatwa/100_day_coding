28b23088e19772cf269d6e6bd6c7bae545b1ca83
suryandini <suryandini04@gmail.com>
2022-10-27 22:09:50 +0800

    arif

---
+++ day11.py
@@ -0,0 +1,23 @@
+# logika if, elif, else
+
+hari = input("masukkan hari : ")
+
+if hari == "senin":
+    print("saya masih libur")
+elif hari == "selasa":
+    print("Dua mata kuliah")
+elif hari == "rabu":
+    print("Tiga mata kuliah")
+elif hari == "kamis":
+    print("Dua mata kuliah")
+elif hari == "jum\'at":
+    print("satu mata kuliah")
+elif hari == "sabtu":
+    print("Libur dari kuliah, tidak dengan Tugas")
+elif hari == "minggu":
+    print("Tidur memikirkan tugas")
+else:
+    print("Maaf anda salah tulis")
+
+        
+
