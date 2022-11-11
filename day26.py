4c465d03b03196357e888085c4f406d357134389
Arifatwa <fatwaarif4@gmail.com>
2022-11-11 18:48:40 +0800

    arifatwa

---
+++ day26.py
@@ -0,0 +1,18 @@
+# belajar perulangan 
+angka = int(input("Masukkan angka : "))
+for i in range (1, angka+1):  
+    print(i*"*")
+
+print("=========================")
+
+i = 0
+for i in range(1,10): # akan mengeluarkan output 1 - 10
+    print(i)
+
+print("=========================")
+
+i = 0
+for i in range(1,10, 2): # akan mengelluarkan output kelipatan dari 1 - 10
+    print(i)
+
+
