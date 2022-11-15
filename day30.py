b6acece8ecdd4e54ddf38a79ce65e1728ca80468
Arifatwa <fatwaarif4gmail.com>
2022-11-15 21:19:16 +0800

    arif

---
+++ day30.py
@@ -0,0 +1,13 @@
+print("========== JUAL MOTOR =========")
+harga = [10000000, 6000000, 5000000]
+jenismotor = input("Masukkan jenis motor : ")
+
+
+if jenismotor == "scorpio" :
+    print(f"harga : {harga[0]}")
+elif jenismotor == "beat" :
+    print(f"harga : {harga[1]}")
+elif jenismotor == "vario":
+    print(f"harga : {harga[2]}")
+else:
+    print("tidak tersedia")
