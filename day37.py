081312f42affd34e21552f533022b167be022902
MUH. ARIFATWA <100128190+Arifatwa@users.noreply.github.com>
2022-11-21 23:14:54 +0800

    Update day37.py

--- day37.py
+++ day37.py
@@ -1,18 +1,39 @@
 # contoh kasus
 
+
+# contoh kasus
+
 gaji = int(input("Masukkan gaji anda : "))
 pekerjaan = input("Masukkan pekerjaan anda : ")
 
-if gaji >= 10000000 and gaji <= 20000000 and pekerjaan == "pns".lower():
-    pajak = gaji / (5 % 100)
-    print(f"Pajak = {pajak}")
-elif gaji >= 20000000 and pekerjaan == "pns".lower():
-    pajak = gaji / (8 % 100)
-    print(f"Pajak = {pajak}")
-elif gaji <= 10000000 and pekerjaan == "petani" or "buruh" or "pengangguran".lower():
-    pajak = 0
-    print(f"Pajak = {pajak} %")
-else :
-    print("TIDAK DI KENAKAN PAJAK")
- 
+if gaji >= 10000000 and pekerjaan == "pns":
+    pajak = gaji * 5 / 100
+    print(f"pajak = {pajak}")
+    print(f"gaji bersih = { gaji - pajak}")
 
+elif gaji >= 20000000 and pekerjaan == "pns":
+    pajak = gaji * 8 / 100
+    print(f"pajak = {pajak}")
+    print(f"gaji bersih = { gaji - pajak}")
+
+elif gaji >= 10000000:
+    pajak = gaji * 2 / 100
+    print(f"pajak = {pajak}")
+    print(f"gaji bersih = { gaji - pajak}")
+
+elif gaji >= 20000000:
+    pajak = gaji * 5 / 100
+    print(f"pajak = {pajak}")
+    print(f"gaji bersih = { gaji - pajak}")
+
+elif pekerjaan == "pns".lower():
+    pajak = gaji * 3 / 100
+    print(f"pajak = {pajak}")
+    print(f"gaji bersih = { gaji - pajak}")
+
+else :
+    pajak = 0
+    print(f"pajak = {pajak}")
+    print(f"gaji bersih = { gaji - pajak}")
+    
+# mohon di terima {'-'}
