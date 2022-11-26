d6b1004b5ed4a1e55cc131bdea21dc6be0a7bc1e
Arifatwa <fatwaarif4gmail.com>
2022-11-26 18:46:14 +0800

    arif

--- day37.py
+++ day37.py
@@ -36,14 +36,5 @@ else :
     pajak = 0
     print(f"pajak = {pajak}")
     print(f"gaji bersih = { gaji - pajak}")
-<<<<<<< HEAD
-
-
-
-
-
-
-=======
     
 # mohon di terima {'-'}
->>>>>>> 2f64899fb7076dd97d9ab1853c3fa7c0f6cf3d16
---
+++ day42.py
@@ -0,0 +1,19 @@
+Buah = 'Melon'
+
+def manisnya():
+    print(Buah)
+
+print('print secara langsung enaknya buah', Buah)
+print('[panggil fungsi manisnya buah]', end=' ')
+manisnya()
+print("\n===================================================")
+
+kota, provinsi = 'Sulbar', 'sulsel'
+def hello ():
+    provinsi = 'Sulbar'
+    print(kota, provinsi)
+    
+print('[PANGGIL FUNGSI hello()]')
+hello()
+print('\n[SECARA LANGSUNG]')
+print(kota, provinsi)
