2bf33aa77b4a68f0031823e78e6bd7467c78e220
Arifatwa <fatwaarif4gmail.com>
2022-12-14 19:32:54 +0800

    arif

---
+++ day60.py
@@ -0,0 +1,23 @@
+print("\n==========PROGRAM PENGSKALAAN=============")
+x1 = int(input("Masukkan nilai x1 : "))
+x2 = int(input("Masukkan nilai x2 : "))
+x3 = int(input("Masukkan nilai x3 : "))
+
+y1 = input("Masukkan nilai y1 : ")
+y2 = int(input("Masukkan nilai y2 : "))
+y3 = int(input("Masukkan nilai y3 : "))
+
+print("\n==========POSISI AWAL=============")
+print("titik1 : ", x1,",",y1)
+print("titik2 : ", x2,",",y2)
+print("titik3 : ", x3,",",y3)
+
+sx = int(input("Masukkan nilai kelas sx : "))
+yx = int(input("Masukkan nilai kelas yx : "))
+
+print("\n==========POSISI AKHIR============")
+
+print("titik1 : ", x1 * sx,",",y1 * yx)
+print("titik2 : ", x2 * sx,",",y2 * yx)
+print("titik3 : ", x3 * sx,",",y3 * yx)
+
