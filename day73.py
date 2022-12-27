1df9c99adc43502b130284d7e78c41dd41c4070a
Arifatwa <fatwaarif4@gmail.com>
2022-12-27 20:27:14 +0800

    arif

---
+++ day73.py
@@ -0,0 +1,25 @@
+#Kelas manusia sebagai "defenisi"
+class Manusia:
+    nama = None
+    usia = None
+    Alamat = None
+
+#membangun instance/variabel sebagai "objek nyata"
+manusia1 = Manusia()
+manusia1.nama = "Arifatwa"
+manusia1.usia = "19 Tahun"
+manusia1.Alamat = "Alamat_Mambi"
+
+manusia2 = Manusia()
+manusia2.nama = "fisah"
+manusia2.usia = "19 Tahun"
+manusia2.Alamat = "Alamat_Mambi"
+
+manusia3 = Manusia()
+manusia3.nama = "Alim"
+manusia3.usia = "19 Tahun"
+manusia3.Alamat = "Alamat_Mambi"
+
+print(manusia1.nama, manusia1.usia, manusia1.Alamat)
+print(manusia2.nama, manusia2.usia, manusia2.Alamat)
+print(manusia3.nama, manusia3.usia, manusia3.Alamat)
