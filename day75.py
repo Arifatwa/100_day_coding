292450b90a90b5d1027974a913fda54a6e1542a5
Arifatwa <fatwaarif4@gmail.com>
2022-12-29 21:34:05 +0800

    arifatwa

---
+++ day75.py
@@ -0,0 +1,25 @@
+'''
+program yang menghitung gaji bersih karyawan setiap bulannya
+dengan ketentuan 
+1. gaji pokok di masukkan melalui input
+2. gaji lembur/jam adalah = 55000
+3. lama lembur di masukka meggunakan fungsi input
+
+'''
+
+gaji_pokok = int(input("Masukkan gaji pokok :"))
+gji_lbrperjam = 55000
+lama_lembur = int(input("Masukkan lama lembur :"))
+
+gaji_bersih = gaji_pokok + gji_lbrperjam * lama_lembur
+
+print("gaji bersih : ", gaji_bersih)
+
+'''maaf kalo salah pak mentor '''
+    
+
+
+
+
+
+
